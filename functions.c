d363ec6939c6a84706144d291f691d0652531ca1
Emre Kılıç <emrekilic1010@gmail.com>
2023-03-18 17:57:19 +0300

    fixed precedence

--- functions.c
+++ functions.c
@@ -140,15 +140,15 @@ int getPrecedence(char* str){
         case '@':
         case '#':
             return 1;
-        case '+':
-            return 2;
-        case '-':
-            return 3;
-        case '*':
-            return 4;
-        case '&':
-            return 5;
         case '|':
+            return 2;
+        case '&':
+            return 3;
+        case '+':
+            return 4;
+        case '-':
+            return 5;
+        case '*':
             return 6;
         case '~':
             return 7;
